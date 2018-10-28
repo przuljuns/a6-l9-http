2011a3d7af3c4cda2422cbb3484a4dc29fcf8588
djordjeprzulj <przulj@gmail.com>
2018-10-28 17:05:42 +0100

    Slajd 8.

--- src/app/services/konto.service.ts
+++ src/app/services/konto.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Konto } from '../domain/konto';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class KontoService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllKontos(): Observable<Konto[]> {
+    return this.httpClient.get<Konto[]>(environment.API_URL + 'konto');
+  }
 }
